cd0ef324e234b21f80187360e2630b5f22246cde
neslihangerek <neslihan.gerek@gmail.com>
2020-03-21 10:28:40 +0100

    correction for wrong files

--- FindShapes.h
+++
@@ -1,6 +0,0 @@
-#pragma once
-
-void checkNeighbors(std::vector<std::vector<unsigned short>>& grid, int i, int j, std::vector<std::pair<int, int>>& shape);
-std::vector<std::pair<int, int>> biggestShape(std::vector<std::vector<unsigned short>>& grid);
-bool checkDigitAndPush(char item, std::vector<unsigned short> &row);
-void exitProgram();
